08b1fdf2e6a60b65b37ae5e2b91deab0ceff8957
Ilango <rajagopalilango@gmail.com>
2023-03-12 14:15:08 +0530

    👌 IMPROVE: Add autoprefixer for tailwind

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -23,6 +23,21 @@ module.exports = {
 				serif: ['Merriweather', 'serif'],
 				serif: ['Orbitron', 'serif'],
 			},
+			animation: {
+				text: 'text 5s ease infinite',
+			},
+			keyframes: {
+				text: {
+					'0%, 100%': {
+						'background-size': '200% 200%',
+						'background-position': 'left center',
+					},
+					'50%': {
+						'background-size': '200% 200%',
+						'background-position': 'right center',
+					},
+				},
+			},
 		},
 	},
 }
