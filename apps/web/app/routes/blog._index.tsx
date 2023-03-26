ab412de3413e6227f35110bf74003b92384b501d
Ilango <rajagopalilango@gmail.com>
2023-03-27 01:38:49 +0530

    👌 IMPROVE: Update blog

--- apps/web/app/routes/blog._index.tsx
+++ apps/web/app/routes/blog._index.tsx
@@ -3,6 +3,7 @@ import { json } from '@remix-run/node'
 import { BlogPost } from '~/lib/types'
 import { formatDate } from '~/lib/utils'
 import { getBlogMdxItems } from '~/services/mdx.server'
+import { Theme, useTheme } from '~/lib/theme'
 
 export async function loader() {
 	const posts = await getBlogMdxItems({ grouped: 'year' })
@@ -11,21 +12,32 @@ export async function loader() {
 
 export default function Blog() {
 	const data = useLoaderData<typeof loader>()
+	const [theme] = useTheme()
 	const { posts } = data
 	return (
 		<div className='container max-w-4xl p-6 lg:py-10 lg:px-0'>
-			<div className='flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8'>
+			<div className='mb-4 flex flex-col items-start gap-4 md:flex-row md:items-center md:justify-between md:gap-8'>
 				<div className='flex-1 space-y-4'>
 					<h1 className='inline-block text-4xl font-extrabold tracking-tight text-slate-900 dark:text-slate-50 lg:text-5xl'>
 						Blog
 					</h1>
 					<p className='text-xl text-slate-700 dark:text-slate-300'>
-						Articles and essays on writing, learning, and Aurelius product
-						updates.
+						Crafts and technical details
 					</p>
 				</div>
+
+				<img
+					className={`${
+						theme === Theme.DARK ? 'invert' : ''
+					} mb-4 w-64`}
+					src={
+						theme === Theme.DARK
+							? '/images/blog.svg'
+							: '/images/blog_light.svg'
+					}
+					alt='writing illustration'
+				/>
 			</div>
-			<hr className='my-8 border-slate-800' />
 			{posts?.length ? (
 				<div className='grid gap-10 sm:grid-cols-2'>
 					{posts.map(
@@ -39,8 +51,13 @@ export default function Blog() {
 									<img
 										src={post.og_image}
 										alt={post.title}
-										className='rounded-md border border-slate-800 bg-slate-800 transition-colors group-hover:border-slate-900'
+										className='rounded-lg border border-slate-800 bg-slate-800 transition-colors group-hover:border-slate-900'
 									/>
+								)}
+								{post.date && (
+									<p className='text-sm text-slate-500 dark:text-slate-500'>
+										{formatDate(post.date)}
+									</p>
 								)}
 								<h2 className='text-2xl font-extrabold text-slate-900 dark:text-slate-50'>
 									{post.title}
@@ -48,11 +65,6 @@ export default function Blog() {
 								{post.description && (
 									<p className='text-slate-700 dark:text-slate-300'>
 										{post.description}
-									</p>
-								)}
-								{post.date && (
-									<p className='text-sm text-slate-500 dark:text-slate-500'>
-										{formatDate(post.date)}
 									</p>
 								)}
 								<Link
