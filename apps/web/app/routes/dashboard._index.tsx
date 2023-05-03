9a3d2d5f570e928b163e37d536e393adaeda5cc8
Ilango <rajagopalilango@gmail.com>
2023-05-03 10:46:33 +0530

    📦 NEW: Send welcome email event to loops

--- apps/web/app/routes/dashboard._index.tsx
+++ apps/web/app/routes/dashboard._index.tsx
@@ -230,7 +230,6 @@ function PostItem({ appUrl, post, username }: PostItemProps) {
 function DeferFallback() {
 	return (
 		<div className='flex items-center space-x-4'>
-			<Skeleton className='h-12 w-12 rounded-full' />
 			<div className='space-y-2'>
 				<Skeleton className='h-4 w-[250px]' />
 				<Skeleton className='h-4 w-[200px]' />
--- apps/web/app/services/auth.server.ts
+++ apps/web/app/services/auth.server.ts
@@ -4,14 +4,32 @@ import invariant from 'tiny-invariant'
 import { sessionStorage } from '~/services/session.server'
 import { sendMagicLinkEmail } from '~/services/email.server'
 import type { User } from '~/models/user.server'
-import { getUserByEmail, createUser } from '~/models/user.server'
+import { createUser, getUserByEmail } from '~/models/user.server'
 
 const MAGIC_LINK_SECRET = process.env.MAGIC_LINK_SECRET
 
-if (!MAGIC_LINK_SECRET)
+if (!MAGIC_LINK_SECRET) {
 	throw new Error('Missing MAGIC_LINK_SECRET environment variable')
+}
 
 export let auth = new Authenticator<User>(sessionStorage)
+
+async function sendSignUpEventToLoops(email: string) {
+	const data = {
+		email,
+		eventName: 'signup',
+	}
+	const response = await fetch('https://app.loops.so/api/v1/events/send', {
+		method: 'POST',
+		body: JSON.stringify(data),
+		headers: {
+			Accept: 'application/json',
+			Authorization: `Bearer ${process.env.LOOPS_API_KEY}`,
+		},
+	})
+	const res = await response.json()
+	return res
+}
 
 auth.use(
 	// @ts-ignore
@@ -31,6 +49,7 @@ auth.use(
 				return user
 			} else {
 				const newUser = await createUser({ email })
+				await sendSignUpEventToLoops(email)
 				return newUser
 			}
 		}
