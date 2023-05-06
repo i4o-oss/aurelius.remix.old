bd19721b8cb6631a1d3fc6619a1956a553dab0e4
Ilango <rajagopalilango@gmail.com>
2023-05-06 22:02:07 +0530

    📦 NEW: Add sitemap and robots.txt

--- apps/web/app/lib/utils.ts
+++ apps/web/app/lib/utils.ts
@@ -1,28 +1,47 @@
 export function formatDate(input: string | number): string {
-	const date = new Date(input)
-	return date.toLocaleDateString('en-US', {
-		month: 'long',
-		day: 'numeric',
-		year: 'numeric',
-	})
+    const date = new Date(input)
+    return date.toLocaleDateString('en-US', {
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric',
+    })
 }
 
 export const getFirstName = (name: string) => {
-	const names = name.split(' ')
-	return names[0]
+    const names = name.split(' ')
+    return names[0]
 }
 
 export const getGreeting = (name: string) => {
-	const hour = new Date().getHours()
-	const firstName = name ? getFirstName(name) : ''
+    const hour = new Date().getHours()
+    const firstName = name ? getFirstName(name) : ''
 
-	if (hour >= 3 && hour < 6) return "Mornin' Sunshine!" // REALLY early
-	if (hour >= 6 && hour < 12)
-		return firstName ? `Good morning, ${firstName}!` : 'Good morning!' // After 6am
-	if (hour >= 12 && hour < 17)
-		return firstName ? `Good afternoon, ${firstName}!` : 'Good afternoon!' // After 12pm
-	if (hour >= 17 && hour < 22)
-		return firstName ? `Good evening, ${firstName}!` : 'Good evening!' // After 5pm
-	if (hour >= 22 || hour < 3)
-		return firstName ? `Go to bed, ${firstName}!` : 'Go to bed!' // After 10pm
+    if (hour >= 3 && hour < 6) return "Mornin' Sunshine!" // REALLY early
+    if (hour >= 6 && hour < 12) {
+        return firstName ? `Good morning, ${firstName}!` : 'Good morning!' // After 6am
+    }
+    if (hour >= 12 && hour < 17) {
+        return firstName ? `Good afternoon, ${firstName}!` : 'Good afternoon!' // After 12pm
+    }
+    if (hour >= 17 && hour < 22) {
+        return firstName ? `Good evening, ${firstName}!` : 'Good evening!' // After 5pm
+    }
+    if (hour >= 22 || hour < 3) {
+        return firstName ? `Go to bed, ${firstName}!` : 'Go to bed!' // After 10pm
+    }
+}
+
+export const toXmlSitemap = (urls: any[]) => {
+    const urlsAsXml = urls
+        .map((url) => `<url><loc>${url}</loc></url>`)
+        .join('\n')
+
+    return `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
+    >
+      ${urlsAsXml}
+    </urlset>`
 }
--- apps/web/app/services/mdx.server.tsx
+++ apps/web/app/services/mdx.server.tsx
@@ -11,118 +11,118 @@ const readdir = promisify(fs.readdir)
 const readFile = promisify(fs.readFile)
 
 async function readFilesInDir(dir: string) {
-	const files = await readdir(dir, { withFileTypes: true })
+    const files = await readdir(dir, { withFileTypes: true })
 
-	const promises = files.map(async (f) => {
-		const filePath = path.join(dir, f.name)
-		const file = await readFile(filePath, 'utf8')
+    const promises = files.map(async (f) => {
+        const filePath = path.join(dir, f.name)
+        const file = await readFile(filePath, 'utf8')
 
-		return file
-	})
+        return file
+    })
 
-	const items = await Promise.all(promises)
+    const items = await Promise.all(promises)
 
-	return items
+    return items
 }
 
 async function getBlogMdxItems({
-	dir = BLOG_POSTS_DIR,
+    dir = BLOG_POSTS_DIR,
 }: GetBlogMdxItemsParams) {
-	const items = await readFilesInDir(dir)
+    const items = await readFilesInDir(dir)
 
-	const posts = items.map((item) => {
-		const content = matter(item)
+    const posts = items.map((item) => {
+        const content = matter(item)
 
-		return {
-			title: content.data.title,
-			description: content.data.excerpt,
-			og_image: content.data.og_image,
-			image: content.data.image,
-			date: content.data.date_published,
-			slug: content.data.slug,
-			draft: content.data.draft,
-			featured: content.data.featured,
-			tag: content.data.tag,
-		}
-	})
+        return {
+            title: content.data.title,
+            description: content.data.excerpt,
+            og_image: content.data.og_image,
+            image: content.data.image,
+            date: content.data.date_published,
+            slug: content.data.slug,
+            draft: content.data.draft,
+            featured: content.data.featured,
+            tag: content.data.tag,
+        }
+    })
 
-	const onlyPublished = posts.filter((post) => !post.draft)
+    const onlyPublished = posts.filter((post) => !post.draft)
 
-	const sortedPosts = onlyPublished.sort(
-		(a, b) => new Date(b.date).valueOf() - new Date(a.date).valueOf()
-	)
+    const sortedPosts = onlyPublished.sort(
+        (a, b) => new Date(b.date).valueOf() - new Date(a.date).valueOf()
+    )
 
-	// if (grouped === 'year') {
-	// 	const articlesGroupedByYear = onlyPublished.reduce(
-	// 		(groups: PostGroup, post: Post) => {
-	// 			const year = post.date.toString().split('-')[0]
-	// 			if (!groups[year]) {
-	// 				groups[year] = []
-	// 			}
+    // if (grouped === 'year') {
+    // 	const articlesGroupedByYear = onlyPublished.reduce(
+    // 		(groups: PostGroup, post: Post) => {
+    // 			const year = post.date.toString().split('-')[0]
+    // 			if (!groups[year]) {
+    // 				groups[year] = []
+    // 			}
 
-	// 			groups[year].push(post)
+    // 			groups[year].push(post)
 
-	// 			return groups
-	// 		},
-	// 		{}
-	// 	)
+    // 			return groups
+    // 		},
+    // 		{}
+    // 	)
 
-	// 	const sortedAndGroupedArticles: PostGroup = {}
+    // 	const sortedAndGroupedArticles: PostGroup = {}
 
-	// 	for (const year in articlesGroupedByYear) {
-	// 		const sortedArticlesList = articlesGroupedByYear[year].sort(
-	// 			(a, b) =>
-	// 				new Date(b.date).valueOf() - new Date(a.date).valueOf()
-	// 		)
+    // 	for (const year in articlesGroupedByYear) {
+    // 		const sortedArticlesList = articlesGroupedByYear[year].sort(
+    // 			(a, b) =>
+    // 				new Date(b.date).valueOf() - new Date(a.date).valueOf()
+    // 		)
 
-	// 		sortedAndGroupedArticles[year] = sortedArticlesList
-	// 	}
+    // 		sortedAndGroupedArticles[year] = sortedArticlesList
+    // 	}
 
-	// 	return sortedAndGroupedArticles
-	// }
+    // 	return sortedAndGroupedArticles
+    // }
 
-	// if (filter === 'latest') {
-	// 	// latest posts
-	// 	return {
-	// 		latest: sortedPosts.slice(0, count),
-	// 	}
-	// } else if (filter === 'featured') {
-	// 	// featured posts
-	// 	return {
-	// 		featured: sortedPosts.filter((post) => post.featured),
-	// 	}
-	// } else if (filter === 'latest,featured') {
-	// 	// latest and featured posts
-	// 	const latest = sortedPosts.slice(0, count)
-	// 	const featured = sortedPosts.filter((post) => post.featured)
-	// 	return {
-	// 		featured,
-	// 		latest,
-	// 	}
-	// }
+    // if (filter === 'latest') {
+    // 	// latest posts
+    // 	return {
+    // 		latest: sortedPosts.slice(0, count),
+    // 	}
+    // } else if (filter === 'featured') {
+    // 	// featured posts
+    // 	return {
+    // 		featured: sortedPosts.filter((post) => post.featured),
+    // 	}
+    // } else if (filter === 'latest,featured') {
+    // 	// latest and featured posts
+    // 	const latest = sortedPosts.slice(0, count)
+    // 	const featured = sortedPosts.filter((post) => post.featured)
+    // 	return {
+    // 		featured,
+    // 		latest,
+    // 	}
+    // }
 
-	return sortedPosts
+    return sortedPosts
 }
 
 async function getMdxFile({ dir = BLOG_POSTS_DIR, slug }: GetMdxFileParams) {
-	const items = await readFilesInDir(dir)
+    const items = await readFilesInDir(dir)
 
-	const posts = items.filter((item) => {
-		const content = matter(item)
+    const posts = items.filter((item) => {
+        const content = matter(item)
 
-		return content.data.slug === slug
-	})
+        return content.data.slug === slug
+    })
 
-	const frontmatter = matter(posts[0])
-	const code = String(
-		await compile(posts[0], {
-			development: false,
-			outputFormat: 'function-body',
-			remarkPlugins: [remarkFrontmatter],
-		})
-	)
+    const frontmatter = matter(posts[0])
+    const code = String(
+        await compile(posts[0], {
+            development: false,
+            outputFormat: 'function-body',
+            remarkPlugins: [remarkFrontmatter],
+        })
+    )
 
-	return { frontmatter, code }
+    return { frontmatter, code }
 }
 
 export { getBlogMdxItems, getMdxFile }
