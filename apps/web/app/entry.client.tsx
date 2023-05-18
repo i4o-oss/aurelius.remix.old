3733564b4d674bc6aa8a9050fecaa89481b33b3d
Ilango <rajagopalilango@gmail.com>
2023-05-18 14:11:39 +0530

    👌 IMPROVE: Initialize amplitude browser sdk

--- apps/web/app/entry.client.tsx
+++ apps/web/app/entry.client.tsx
@@ -1,38 +1,52 @@
 import { RemixBrowser, useLocation, useMatches } from '@remix-run/react'
 import { startTransition, StrictMode, useEffect } from 'react'
-import * as Sentry from '@sentry/remix'
 import { hydrateRoot } from 'react-dom/client'
+import * as Sentry from '@sentry/remix'
+import * as amplitude from '@amplitude/analytics-browser'
 
+// initialize sentry
 Sentry.init({
-	dsn: 'https://56e73be27d7845a681a3d7fcc0fcbe8d:ba0b10913fae423cafde214e9c5ac9ef@o4504987249803264.ingest.sentry.io/4504987253866496',
-	enabled: process.env.NODE_ENV !== 'development',
-	tracesSampleRate: 1,
-	integrations: [
-		new Sentry.BrowserTracing({
-			routingInstrumentation: Sentry.remixRouterInstrumentation(
-				useEffect,
-				useLocation,
-				useMatches
-			),
-		}),
-	],
+    dsn: 'https://56e73be27d7845a681a3d7fcc0fcbe8d:ba0b10913fae423cafde214e9c5ac9ef@o4504987249803264.ingest.sentry.io/4504987253866496',
+    enabled: process.env.NODE_ENV !== 'development',
+    tracesSampleRate: 1,
+    integrations: [
+        new Sentry.BrowserTracing({
+            routingInstrumentation: Sentry.remixRouterInstrumentation(
+                useEffect,
+                useLocation,
+                useMatches
+            ),
+        }),
+    ],
 })
 
+// initialize amplitude but only on production
+if (process.env.NODE_ENV === 'production') {
+    amplitude.init('1c06c1f6182a4603d951153db30574bd', undefined, {
+        defaultTracking: {
+            sessions: true,
+            pageViews: true,
+            formInteractions: true,
+            fileDownloads: true,
+        },
+    })
+}
+
 function hydrate() {
-	startTransition(() => {
-		hydrateRoot(
-			document,
-			<StrictMode>
-				<RemixBrowser />
-			</StrictMode>
-		)
-	})
+    startTransition(() => {
+        hydrateRoot(
+            document,
+            <StrictMode>
+                <RemixBrowser />
+            </StrictMode>
+        )
+    })
 }
 
 if (typeof requestIdleCallback === 'function') {
-	requestIdleCallback(hydrate)
+    requestIdleCallback(hydrate)
 } else {
-	// Safari doesn't support requestIdleCallback
-	// https://caniuse.com/requestidlecallback
-	setTimeout(hydrate, 1)
+    // Safari doesn't support requestIdleCallback
+    // https://caniuse.com/requestidlecallback
+    setTimeout(hydrate, 1)
 }
