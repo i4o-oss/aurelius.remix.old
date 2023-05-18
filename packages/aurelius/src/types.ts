dac3445821308f65e800466e4fd2314f15f8aeff
Ilango <rajagopalilango@gmail.com>
2023-05-18 14:11:16 +0530

    📦 NEW: Add events type for amplitude

--- packages/aurelius/src/types.ts
+++ packages/aurelius/src/types.ts
@@ -69,14 +69,26 @@ export interface SettingsData {
 	user?: ProfileSettings
 }
 
-export const EventType = {
-	NEW_POST_CLICKED: 'new_post_clicked',
-	NEW_WRITING_SESSION_CLICKED: 'new_writing_session_clicked',
-	WRITING_SESSION_STARTED: 'writing_session_started',
-	WRITING_SESSION_FINISHED: 'writing_session_finished',
-	IMAGE_EXPORTED: 'image_exported',
-	MARKDOWN_EXPORTED: 'markdown_exported',
-    POST_PUBLISHED: 'post_published'
+// export const EventType = {
+// 	NEW_POST_CLICKED: 'new_post_clicked',
+// 	NEW_WRITING_SESSION_CLICKED: 'new_writing_session_clicked',
+// 	WRITING_SESSION_STARTED: 'writing_session_started',
+// 	WRITING_SESSION_FINISHED: 'writing_session_finished',
+// 	IMAGE_EXPORTED: 'image_exported',
+// 	MARKDOWN_EXPORTED: 'markdown_exported',
+//     POST_PUBLISHED: 'post_published'
+// } as const
+//
+// export type EventType = (typeof EventType)[keyof typeof EventType]
+
+export const AmplitudeEventType = {
+	POST_CREATED: 'Post Created',
+    POST_PUBLISHED: 'Post Published',
+	WRITING_SESSION_CLICKED: 'Writing Session Clicked',
+	WRITING_SESSION_STARTED: 'Writing Session Started',
+	WRITING_SESSION_FINISHED: 'Writing Session Finished',
+	IMAGE_EXPORTED: 'Image Exported',
+	MARKDOWN_EXPORTED: 'Markdown Exported',
 } as const
 
-export type EventType = (typeof EventType)[keyof typeof EventType]
+export type AmplitudeEventType = (typeof AmplitudeEventType)[keyof typeof AmplitudeEventType]
