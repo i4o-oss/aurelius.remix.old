784deb797cd28d3779f1869d9b2573df5cb59579
Ilango <rajagopalilango@gmail.com>
2023-06-16 09:03:17 +0530

    👌 IMPROVE: Comment out oembed related code cuz they were throwing errors with the new remix version

--- apps/web/app/routes/api.oembed.tsx
+++ apps/web/app/routes/api.oembed.tsx
@@ -1,31 +1,31 @@
 import type { LoaderArgs } from '@remix-run/node'
 import { json } from '@remix-run/node'
-import {
-	findUrlWithProvider,
-	getOembed,
-	getOembedDataFromScraper,
-	getOembedFromLink,
-} from '@aurelius/writer'
+// import {
+// 	findUrlWithProvider,
+// 	getOembed,
+// 	getOembedDataFromScraper,
+// 	getOembedFromLink,
+// } from '@aurelius/writer'
 
 export async function loader({ request }: LoaderArgs) {
-	const requestUrl = new URL(request.url)
-	const _url = requestUrl.searchParams.get('url')
-	const decodedUrl = decodeURIComponent(_url as string)
+	// const requestUrl = new URL(request.url)
+	// const _url = requestUrl.searchParams.get('url')
+	// const decodedUrl = decodeURIComponent(_url as string)
 
 	// check if given url is in the oembed list
 	// if available, use extract to get oembed data
-	const { url, provider } = findUrlWithProvider(decodedUrl as string)
-	let oembed = null
-	if (provider) {
-		oembed = await getOembed(url)
-		if (oembed) return json({ oembed }, 200)
-	}
-
-	oembed = await getOembedFromLink(decodedUrl)
-	if (oembed) return json({ oembed }, 200)
-
-	oembed = await getOembedDataFromScraper(decodedUrl)
-	if (oembed) return json({ oembed }, 200)
+	// const { url, provider } = findUrlWithProvider(decodedUrl as string)
+	// let oembed = null
+	// if (provider) {
+	// 	oembed = await getOembed(url)
+	// 	if (oembed) return json({ oembed }, 200)
+	// }
+	//
+	// oembed = await getOembedFromLink(decodedUrl)
+	// if (oembed) return json({ oembed }, 200)
+	//
+	// oembed = await getOembedDataFromScraper(decodedUrl)
+	// if (oembed) return json({ oembed }, 200)
 
 	return json({})
 }
