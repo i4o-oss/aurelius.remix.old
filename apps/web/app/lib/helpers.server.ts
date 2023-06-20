b3f50c0f53c543ba89ed26536bbdc210ac329ea1
Ilango <rajagopalilango@gmail.com>
2023-06-20 13:17:49 +0530

    🐛 FIX: Maybe fix puppeteer browser launch issue on railway

--- apps/web/app/lib/helpers.server.ts
+++ apps/web/app/lib/helpers.server.ts
@@ -1,5 +1,20 @@
-import puppeteer from "puppeteer";
+import puppeteer from 'puppeteer'
+
+const CHROME_DEFAULT_ARGS = [
+    '--disable-client-side-phishing-detection',
+    '--disable-default-apps',
+    '--disable-extensions',
+    '--disable-features=InterestFeedContentSuggestions',
+    '--disable-features=Translate',
+    '--hide-scrollbars',
+    '--mute-audio',
+    '--no-default-browser-check',
+    '--no-first-run',
+]
 
 export async function getBrowser() {
-    return puppeteer.launch({ headless: 'new' })
+    return puppeteer.launch({
+        headless: 'new',
+        args: CHROME_DEFAULT_ARGS,
+    })
 }
