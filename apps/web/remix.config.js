4713ab31f6eadebd48e0f0ca90be39cdc907e53e
Ilango <rajagopalilango@gmail.com>
2023-06-28 09:46:31 +0530

    👌 IMPROVE: Opt-in to more v2 remix features

--- apps/web/remix.config.js
+++ apps/web/remix.config.js
@@ -6,7 +6,10 @@ module.exports = {
     tailwind: true,
     future: {
         v2_dev: true,
+        v2_errorBoundary: true,
+        v2_headers: true,
         v2_meta: true,
+        v2_normalizeFormMethod: true,
         v2_routeConvention: true,
     },
     // When running locally in development mode, we use the built in remix
